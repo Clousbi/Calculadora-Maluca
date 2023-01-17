21ea4e361c6edd16b7c6baae642494af5e5f176e
Clousbi <biancamthope@gmail.com>
2023-01-17 14:43:35 -0300

    main

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 var resultado = document.getElementById('resultado').value;
 var val = 0.0;
 //
-function insert(num) { //aparece o número na tabela
+function insert(num) { 
     var numero = document.getElementById('resultado').value;
     document.getElementById('resultado').value = numero + num;
 }
 //
-function clean() { // limpa a operação inteira
+function clean() { 
     document.getElementById('resultado').value = "";
     document.getElementById('operacaoJogo').value = "";
 }
 //
-function backspace() { // limpa somente um número
+function backspace() { 
     var resultado = document.getElementById('resultado').value;
     document.getElementById('resultado').value = resultado.substring(0, resultado.length - 1);
 }
 //
-function result() {//função da operação 
+function result() {
     var resultado = document.getElementById('resultado').value;
     if (resultado) {
         document.getElementById('resultado').value = eval(resultado);
@@ -78,33 +78,33 @@ function alteraSinal() {
 //
 //calculadora maluca
 var resultado;
-var valor = 0; // valor da operação da calculadora 
+var valor = 0; 
 var decimal;
-var interval; //usado na função do cronometro
+var interval; 
 var vida1 = document.getElementById('vida1');
 var vida2 = document.getElementById('vida2');
 var vida3 = document.getElementById('vida3');
 var i = 3;
+var pontos_valor = document.getElementById('pontos').value = '0';
 //
-function telaJogo(){ // quando é acionado o jogo, aparece essa função
-    clean(); // limpa o input 'resultado'
-    calculateM(10); // chama a função de calcular a operação do jogo
-    configTimer(); // chama o cronometro
-    var pontos_valor = document.getElementById('pontos').value = '0'; // os pontos tem o valor de 0 (do input)
+function telaJogo(){ 
+    clean(); 
+    calculateM(10);
+    configTimer(); 
     //
-    var bloco = document.getElementById('operacaoJogo'); // aparece o input bloco
+    var bloco = document.getElementById('operacaoJogo'); 
     bloco.style.display = "block";
-    var tabela1 = document.getElementById('tabela1'); // aparece a tabela 1
+    var tabela1 = document.getElementById('tabela1'); 
     tabela1.style.display = "block";
-    var tabela2 = document.getElementById('tabela2'); // aparece a tabela 2
+    var tabela2 = document.getElementById('tabela2'); 
     tabela2.style.display = "block";
-    var volta = document.getElementById('voltar'); // aparece o botão de voltar
+    var volta = document.getElementById('voltar'); 
     volta.style.display = "block";
-    var placar = document.getElementById('pontosJogo'); // aparece o "pontos"
+    var placar = document.getElementById('pontosJogo'); 
     placar.style.display = "contents";
-    var ponto = document.getElementById('pontos'); // aparece o placar de pontos
+    var ponto = document.getElementById('pontos'); 
     ponto.style.display = "block";
-    var btnCalM = document.getElementById('calM'); // some o botão de "calculadora maluca"
+    var btnCalM = document.getElementById('calM'); 
     btnCalM.style.display = "none";
     var vida1 = document.getElementById('vida1');
     vida1.style.display = "block";
@@ -120,16 +120,7 @@ function telaJogo(){ // quando é acionado o jogo, aparece essa função
             if (valor == (resultado) || decimal == (resultado)) { 
                 pontuacao(); 
                 pontos_valor++;
-                if (pontos_valor <= 10){
-                    calculateM(10);
-                }else if(pontos_valor > 10 && pontos_valor <= 20){
-                    calculateMMedioDificil(10); 
-                }
-                else if(pontos_valor > 20 && pontos_valor <= 50){
-                    calculateMMedioDificil(30);
-                }else{
-                    calculateMImpossivel(100);
-                }
+                sistemNivel(pontos_valor);
             }
             else { 
                 
@@ -137,12 +128,12 @@ function telaJogo(){ // quando é acionado o jogo, aparece essa função
                     vida1.innerHTML = document.getElementById('cv1').innerHTML;
                     vida1.value = document.getElementById('cv1').value;
                     i--;
-                    calculateM(10);
+                    sistemNivel(pontos_valor);
                 }else if (i == 2){
                     vida2.innerHTML = document.getElementById('cv2').innerHTML;
                     vida2.value = document.getElementById('cv2').value;
                     i--;
-                    calculateM(10);
+                    sistemNivel(pontos_valor);
                 }else if (i == 1){
                     vida3.innerHTML = document.getElementById('cv3').innerHTML;
                     vida3.value = document.getElementById('cv3').value;
@@ -252,58 +243,71 @@ function calculateMImpossivel(operador){
     }
 }
 //
-function voltar() { //função do botão de voltar
-    var volta = document.getElementById('voltar'); // cria uma variável que é igual ao botão voltar
-    volta.addEventListener('click', document.location.reload(true)); // ao clicar o botão reinicia o site
+function voltar() { 
+    var volta = document.getElementById('voltar'); 
+    volta.addEventListener('click', document.location.reload(true)); 
 }
 //
-function numeroOperacao(num_random) { // randoniza 
+function numeroOperacao(num_random) { 
     return Math.floor(Math.random() * num_random + 1);
 }
 //
-function operacao() { // randoniza de 1 a 4
+function operacao() { 
     return Math.floor(Math.random() * 4 + 1);
 }
 //
 function pontuacao() { 
-    var ponto = parseInt(document.getElementById('pontos').value); // o valor da variavel ponto é igual ao valor do input pontos, que passa para inteiro
-    ponto++; // ponto = ponto + 1
-    document.getElementById('pontos').value = ponto; // os pontos aparecem no input
-    clearInterval(interval); // limpa o cronometro 
-    configTimer(); // reinicia o cronometro
+    var ponto = parseInt(document.getElementById('pontos').value); 
+    ponto++; 
+    document.getElementById('pontos').value = ponto;
+    document.getElementById('pontos').innerHTML = ponto;
+    clearInterval(interval);  
+    configTimer(); 
 }
 //
-function finalizacao(tipo){ // função de fim de jogo
-    var pontoFinal = document.getElementById('pontos').value; // pega os pontos do input e poe numa variavel
-    if(tipo == 0){ // se o tipo for 0, é fim de jogo por tempo
-        alert('Seu tempo acabou, sua pontuação foi: ' + pontoFinal); // alerta para o jogador que terminou o jogo
-        window.location.reload(true); // reinicia o site
-    }else{ // se o tipo for 1, é fim de jogo por erro de resposta
-        alert('Suas vidas acabaram, sua pontuação foi: ' + pontoFinal); // alerta para o jogador que terminou o jogo
-        window.location.reload(true); // reinicia o site
+function finalizacao(tipo){ 
+    var pontoFinal = document.getElementById('pontos').value; 
+    if(tipo == 0){ 
+        alert('Seu tempo acabou, sua pontuação foi: ' + pontoFinal); 
+        window.location.reload(true); 
+    }else{ 
+        alert('Suas vidas acabaram, sua pontuação foi: ' + pontoFinal); 
+        window.location.reload(true);
     }
 }
 //
-function startTimer(duration, display) { // função do cronometro 
-    var timer = duration, minutes, seconds; // duração, null, null
-    interval = setInterval(function () { // setInterval é que ocorre a cada 1000 milesimos ou 1 segundo
-        minutes = parseInt(timer / 60, 10); //o tempo é dividido por 60, na base 10
-        seconds = parseInt(timer % 60, 10); // o resto da divisão por 60, na base 10, são os segundos
-        minutes = minutes < 10 ? "0" + minutes : minutes; // mostra na tela os com uma condição de, o número menor que 10, aparece o 0 na frente, exemplo: 09, 08.
-        seconds = seconds < 10 ? "0" + seconds : seconds; // mostra na tela os com uma condição de, o número menor que 10, aparece o 0 na frente, exemplo: 09, 08.
-        display.textContent = minutes + ":" + seconds; // mostra na tela 00:00
-        if (--timer < 0) { // caso o tempo zere
+function startTimer(duration, display) { 
+    var timer = duration, minutes, seconds;
+    interval = setInterval(function () { 
+        minutes = parseInt(timer / 60, 10); 
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds; 
+        if (--timer < 0) { 
             timer = duration;
-            display.style.display = 'none'; // display some
-            finalizacao(0); // finaliza por tempo esgotado
+            display.style.display = 'none'; 
+            finalizacao(0); 
         }
     }, 1000);
 }
 //
 function configTimer() {
-    var duration = 60 * 3; // Converter para segundos
-    display = document.querySelector('#timer'); // selecionando o timer
-    startTimer(duration, display); // iniciando o timer
+    var duration = 60 * 3; 
+    display = document.querySelector('#timer'); 
+    startTimer(duration, display); 
 };
 //
+function sistemNivel(pontos_valor){
+    if (pontos_valor <= 10){
+        calculateM(10);
+    }else if(pontos_valor > 10 && pontos_valor <= 20){
+        calculateMMedioDificil(10); 
+    }
+    else if(pontos_valor > 20 && pontos_valor <= 50){
+        calculateMMedioDificil(30);
+    }else{
+        calculateMImpossivel(100);
+    }
+}
 
